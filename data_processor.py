4db6878d87627722cea063b7416fbb1a7fe9cc27
Manthan Surjuse <manthansje10@gmail.com>
2026-01-04 19:00:25 -0600

    Updated data_processor.py

--- data_processor.py
+++ data_processor.py
@@ -15,7 +15,7 @@ class DataProcessor:
         """Extract text from PDF files"""
         try:
             with open(file_path, 'rb') as file:
-                reader = PyPDF2.PdfReader(file)
+                reader = PdfReader(file)
                 text = ""
                 for page in reader.pages:
                     text += page.extract_text()
